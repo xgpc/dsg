9204d0778d861bd5f970df4da9376cafc81ba616
xgpc <82471454@qq.com>
2022-04-01 20:41:08 +0800

    feat: struct.go 添加结构体转map功能

---
+++ util/struct.go
@@ -0,0 +1,24 @@
+// Package util
+// @Author:        asus
+// @Description:   $
+// @File:          struct
+// @Data:          2022/4/119:31
+//
+package util
+
+import "reflect"
+
+func StructToMapByRef(obj interface{}) (data map[string]interface{}) {
+	data = make(map[string]interface{})
+	objT := reflect.TypeOf(obj)
+	objV := reflect.ValueOf(obj)
+	if objT.Kind() == reflect.Interface || objT.Kind() == reflect.Ptr {
+		objT = objT.Elem()
+		objV = objV.Elem()
+	}
+
+	for i := 0; i < objT.NumField(); i++ {
+		data[objT.Field(i).Name] = objV.Field(i).Interface()
+	}
+	return
+}
