1fcc6733b7e5f2ea1fb46fe85d2f46cd9ece22ed
sfr mac <82471454@qq.com>
2022-09-07 17:46:06 +0800

    feat: 添加配置文件读取功能

---
+++ util/conf/conf.go
@@ -0,0 +1,38 @@
+/**
+ * @Author: smono
+ * @Description:
+ * @File:  conf
+ * @Version: 1.0.0
+ * @Date: 2022/6/30 9:16
+ */
+
+package conf
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"path"
+)
+
+const defConfigPath = "config.yaml"
+
+// LoadConf 读取Yaml配置文件
+func LoadConf(out interface{}, fileName ...string) {
+
+	var configPath string
+	if len(fileName) == 0 {
+		configPath = defConfigPath
+	} else {
+		configPath = path.Join(fileName...)
+	}
+
+	yamlFile, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = yaml.Unmarshal(yamlFile, out)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
