397eaef934bd763cf91b7283c3927b7dcb6c0cac
尚富荣 <82471454@qq.com>
2023-01-14 16:25:08 +0800

    refactor: 去掉grpcService, 该功能已经迁移

--- service/grpcService/proto/gen.sh
+++
@@ -1,5 +0,0 @@
-
-protoc --go_out=. --go-grpc_out=. base.proto -I .
-
-# 可在根目录下运行, 并生成文件到proto所在目录
-# protoc --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative .\service\grpcService\proto\base.proto
