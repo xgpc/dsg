e69dd1f2725faf5beeb4afe89264695395c324d3
dsg <82471454@qq.com>
2023-07-10 10:49:01 +0800

    feat: v2.0.11

--- load.go
+++ load.go
@@ -2,7 +2,8 @@ package dsg
 
 import (
 	"github.com/kataras/iris/v12"
-	"github.com/xgpc/dsg/v2/env"
+	"strconv"
+
 	"github.com/xgpc/dsg/v2/pkg/util"
 	"log"
 )
@@ -11,18 +12,18 @@ func LoadYml(out interface{}, configPath string) {
 	util.LoadYmlConf(out, configPath)
 }
 
-func Load(conf *env.Conf, configPath string) {
+func Load(configPath string) {
 	// load config
-	util.LoadYmlConf(conf, configPath)
+	util.LoadYmlConf(Conf, configPath)
 }
 
 // Listening 开始监听端口
-func Listening(app *iris.Application) {
+func Listening(Port int, Tls string, app *iris.Application) {
 	// 开始监听Http(s)
 	log.Println("服务启动成功")
-	host := ":" + env.Config.App.Port
-	if env.Config.App.TLS != "" {
-		tlsPrefix := env.Config.App.TLS
+	host := ":" + strconv.Itoa(Port)
+	if Tls != "" {
+		tlsPrefix := Tls
 		_ = app.Run(iris.TLS(host, tlsPrefix+".pem", tlsPrefix+".key"))
 	} else {
 		_ = app.Run(iris.Addr(host))
