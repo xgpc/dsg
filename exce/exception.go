2a43d6f472b23fba0d1c270172d62d98a2d65d4c
smono <82471454@qq.com>
2024-05-20 15:42:09 +0800

    feat: 更新常用code值

--- exce/exception.go
+++ exce/exception.go
@@ -20,35 +20,79 @@ func (d DsgError) Code() int {
 const (
 	CodeOK = 0
 )
+
 const (
 	CodeSysBusy DsgError = 1001 + iota
+	CodeSysConfigError
+	CodeSysDatabaseError
+	CodeSysCacheError
+	CodeSysFileError
+	CodeSysDependencyError
 )
 
 // 用户类
 const (
-	CodeUserError DsgError = 2001 + iota
+	CodeUserError = 2001 + iota
 	CodeUserNoLogin
 	CodeUserNoAuth
-	CodeUserTokenError // token错误
+	CodeUserTokenError
+	CodeUserPasswordError
+	CodePermissionError
+	CodePermissionDenied
 )
 
+// 请求相关
 const (
-	//	请求
-
 	CodeRequestError DsgError = 3001 + iota
+	CodeRequestTimeout
+	CodeRequestTooMany
+	CodeRequestAPINotFound
+	CodeRequestServiceNotAvailable
+)
+
+// 数据库相关
+const (
+	CodeDBError DsgError = 4001 + iota
+	CodeDBInsertError
+	CodeDBUpdateError
+	CodeDBDeleteError
+	CodeDBQueryError
+)
+
+// CodeUnknownError 其他通用错误
+const (
+	CodeUnknownError DsgError = 6001
 )
 
 var ErrString = map[DsgError]string{
-	CodeOK: "ok",
-	//系统
-	CodeSysBusy: "[error] 系统繁忙，请稍后重试",
+	CodeOK:                 "ok",
+	CodeSysBusy:            "[error] 系统繁忙，请稍后重试",
+	CodeSysConfigError:     "[error] 系统配置错误",
+	CodeSysDatabaseError:   "[error] 数据库访问错误",
+	CodeSysCacheError:      "[error] 缓存访问错误",
+	CodeSysFileError:       "[error] 文件操作错误",
+	CodeSysDependencyError: "[error] 依赖服务不可用",
+	CodeUserError:          "[error] 用户类错误",
+	CodeUserNoLogin:        "[error] 用户未登录",
+	CodeUserNoAuth:         "[error] 抱歉，您的角色没有此功能的操作权限，请联系技术人员(开发人员级别权限)",
+	CodeUserTokenError:     "[error] token无效或已过期",
+	CodeUserPasswordError:  "[error] 密码错误",
+	CodePermissionError:    "[error] 权限错误(接口权限)",
+	CodePermissionDenied:   "[error] 权限被拒绝(层级权限)",
 
-	//	用户
-	CodeUserError:   "[error] 用户类错误",
-	CodeUserNoLogin: "[error] 请先登录",
-	CodeUserNoAuth:  "[error] 抱歉，您的角色没有此功能的操作权限，请联系技术人员",
+	CodeRequestError:               "[error] 请求参数错误(给出明确的错误问题)",
+	CodeRequestTimeout:             "[error] 请求超时",
+	CodeRequestTooMany:             "[error] 请求过多",
+	CodeRequestAPINotFound:         "[error] 请求接口不存在",
+	CodeRequestServiceNotAvailable: "[error] 请求服务不存在",
 
-	CodeRequestError: "[error] 请求错误，请检查参数",
+	CodeDBError:       "[error] 数据库错误",
+	CodeDBInsertError: "[error] 数据插入失败",
+	CodeDBUpdateError: "[error] 数据更新失败",
+	CodeDBDeleteError: "[error] 数据删除失败",
+	CodeDBQueryError:  "[error] 数据查询失败",
+
+	CodeUnknownError: "[error] 未知错误",
 }
 
 type SysException struct {
@@ -56,7 +100,7 @@ type SysException struct {
 	Msg  string
 }
 
-// ThrowSys 抛出错误
+// ThrowSys 抛出自定义错误
 func ThrowSys(err error, args ...interface{}) {
 	num := len(args)
 	var e SysException
